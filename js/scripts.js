298f85a71b7ea88a461b6cc4e6a3ad611ba61508
Dylan Manley and Lela Smith <lela@>
2019-10-22 10:50:48 -0700

    add buttons to change background color.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#clown-hidden").toggle();
-    $("#clown-showing").toggle();
+    $("#clown-hidden").slideToggle();
+    $("#clown-showing").slideToggle();
+  });
+});
+$(document).ready(function() {
+  $("button#orange").click(function() {
+    $("body").removeClass();
+    $("body").addClass("orange-background");
+  });
+
+  $("button#purp").click(function() {
+    $("body").removeClass();
+    $("body").addClass("purp-background");
+  });
+
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background");
   });
 });
